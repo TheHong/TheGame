783d802378acccef38d53e8975730c4daaa697f9
Chi-Hong Cheung <chcheung.office@gmail.com>
2020-05-24 15:23:59 -0400

    Edit button behaviour

--- lib/widgets/note_icon.dart
+++ lib/widgets/note_icon.dart
@@ -15,25 +15,34 @@ class _NoteIconState extends State<NoteIcon> {
     return Consumer<GameInfo>(
       builder: (context, gameInfo, child) {
         return Container(
-          color: Colors.indigo,
+            color: gameInfo.isDebugMode ? Colors.indigo : Colors.transparent,
             child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: IconButton(
-            icon: Icon(
-              Icons.music_note,
-              color: gameInfo.isRoundDone
-                  ? (gameInfo.isCorrect ? Colors.green : Colors.red)
-                  : Colors.blue,
-            ),
-            iconSize: 100,
-            onPressed: () {
-              // Play note
-              gameInfo.notePlayer.play();
-              print(
-                  "${gameInfo.notePlayer.currNote} (${gameInfo.notePlayer.currNoteAsStr})");
-            },
-          ),
-        ));
+              padding: const EdgeInsets.all(50.0),
+              child: OutlineButton(
+                padding: EdgeInsets.all(25),
+                shape: CircleBorder(),
+                child: Icon(
+                  Icons.music_note,
+                  size: 100,
+                  // Inactive: Black
+                  // Awaiting user answer: Blue
+                  // Displaying result: Green or Red
+                  color: !gameInfo.keyboard.isActive && !gameInfo.isRoundDone
+                      ? Colors.black
+                      : gameInfo.isRoundDone
+                          ? (gameInfo.isCorrect ? Colors.green : Colors.red)
+                          : Colors.blue,
+                ),
+                onPressed: !gameInfo.keyboard.isActive
+                    ? null
+                    : () {
+                        // Play note
+                        gameInfo.notePlayer.play();
+                        print(
+                            "${gameInfo.notePlayer.currNote} (${gameInfo.notePlayer.currNoteAsStr})");
+                      },
+              ),
+            ));
       },
     );
   }
