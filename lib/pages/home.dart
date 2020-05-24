83f9e609572fff436380ae090ec1efa7d48e2859
Chi-Hong Cheung <chcheung.office@gmail.com>
2020-05-24 16:03:31 -0400

    Create home screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,44 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Color(0xFF21BFBD),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 0, top: 30.0),
+              child: Text(
+                "The Game",
+                style: TextStyle(
+                  fontFamily: "Montserrat",
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 18.0),
+              child: Text(
+                "by The Hong",
+                style: TextStyle(
+                  fontFamily: "Montserrat",
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(75.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
