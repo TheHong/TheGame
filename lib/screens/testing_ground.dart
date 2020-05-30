1e31e86c43bf74ba6f18baf7854988cd1b41d4e0
Chi-Hong Cheung <chcheung.office@gmail.com>
2020-05-30 18:10:45 -0400

    Make testing_ground even safer (to prevent overwrite)

--- lib/screens/testing_ground.dart
+++ lib/screens/testing_ground.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:game_app/models/constants.dart';
 import 'package:game_app/models/results.dart';
-import 'package:game_app/services/database.dart';
 
 class TestingGround extends StatefulWidget {
   @override
@@ -61,4 +60,33 @@ class DatabaseServiceTest {
       },
     );
   }
+}
+
+List<Result> getResultsFromAsyncSnapshot(String game, AsyncSnapshot snapshot) {
+  // Looking for data
+  if (!snapshot.hasData) return [];
+
+  // Extracting the results from the snapshot
+  DocumentSnapshot resultsDocument = snapshot.data;
+  return getResultsFromDocSnapshot(game, resultsDocument);
+}
+
+List<Result> getResultsFromDocSnapshot(String game, DocumentSnapshot snapshot) {
+  Map results = snapshot.data;
+  if (!results.containsKey(game)) return []; // If no data for game
+
+  // Storing and returning the results
+  List gameResults = results[game];
+  List<Result> historicalResults = List();
+  for (int i = 0; i < gameResults.length; i++) {
+    historicalResults.add(
+      Result(
+        game: game,
+        name: gameResults[i]["name"],
+        score: gameResults[i]["score"] * 1.0,
+        timestamp: gameResults[i]["timestamp"],
+      ),
+    );
+  }
+  return historicalResults;
 }
