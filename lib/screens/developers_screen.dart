a357851fc609bc6ee9bcf4467721ad2db0b543a7
Chi-Hong Cheung <chcheung.office@gmail.com>
2020-06-01 14:39:23 -0400

    Complete the backup functionality

--- lib/screens/developers_screen.dart
+++ lib/screens/developers_screen.dart
@@ -87,7 +87,7 @@ class _DevelopersScreenState extends State<DevelopersScreen> {
           "Backup",
           style: TextStyle(
             color: backupLocation.isEmpty || _isPressed
-                ? Colors.black26
+                ? Colors.black12
                 : Colors.green,
           ),
         ),
@@ -115,101 +115,17 @@ class DatabaseBackupService {
   final DocumentReference onlineStatsDoc = Firestore.instance
       .collection(Constant.FIREBASE_COLLECTION_NAME)
       .document(Constant.FIREBASE_STATS_DOCUMENT_NAME);
-  final DocumentReference backupDataDoc = Firestore.instance
-      .collection(Constant.FIREBASE_COLLECTION_NAME)
-      .document("backup1");
 
   Future backup(String backupLocation) async {
-    Map backupData = {"Backup Time": Timestamp.now()};
+    final DocumentReference backupDataDoc = Firestore.instance
+        .collection(Constant.FIREBASE_COLLECTION_NAME)
+        .document(backupLocation);
+    Map<String, dynamic> backupData = {"Backup Time": Timestamp.now()};
     DocumentSnapshot resultsDocument = await onlineDataDoc.get();
-    // backupData.addAll(resultsDocument.data);
+    backupData.addAll(resultsDocument.data);
     DocumentSnapshot statsDocument = await onlineStatsDoc.get();
     for (String key in statsDocument.data.keys)
       backupData["$key Stats"] = statsDocument.data[key];
     return await backupDataDoc.setData(backupData);
   }
 }
-
-// // Copyright 2019 The Flutter team. All rights reserved.
-// // Use of this source code is governed by a BSD-style license that can be
-// // found in the LICENSE file.
-
-// import 'package:flutter/material.dart';
-
-// import 'package:gallery/l10n/gallery_localizations.dart';
-
-// // This entire list item is a PopupMenuButton. Tapping anywhere shows
-// // a menu whose current value is highlighted and aligned over the
-// // list item's center line.
-// class _SimpleMenuDemo extends StatefulWidget {
-//   const _SimpleMenuDemo({Key key, this.showInSnackBar}) : super(key: key);
-
-//   final void Function(String value) showInSnackBar;
-
-//   @override
-//   _SimpleMenuDemoState createState() => _SimpleMenuDemoState();
-// }
-
-// class _SimpleMenuDemoState extends State<_SimpleMenuDemo> {
-//   SimpleValue _simpleValue;
-
-//   void showAndSetMenuSelection(BuildContext context, SimpleValue value) {
-//     setState(() {
-//       _simpleValue = value;
-//     });
-//     widget.showInSnackBar(
-//       GalleryLocalizations.of(context)
-//           .demoMenuSelected(simpleValueToString(context, value)),
-//     );
-//   }
-
-//   String simpleValueToString(BuildContext context, SimpleValue value) => {
-//         SimpleValue.one: GalleryLocalizations.of(context).demoMenuItemValueOne,
-//         SimpleValue.two: GalleryLocalizations.of(context).demoMenuItemValueTwo,
-//         SimpleValue.three:
-//             GalleryLocalizations.of(context).demoMenuItemValueThree,
-//       }[value];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _simpleValue = SimpleValue.two;
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return PopupMenuButton<SimpleValue>(
-//       padding: EdgeInsets.zero,
-//       initialValue: _simpleValue,
-//       onSelected: (value) => showAndSetMenuSelection(context, value),
-//       child: ListTile(
-//         title: Text(
-//             GalleryLocalizations.of(context).demoMenuAnItemWithASimpleMenu),
-//         subtitle: Text(simpleValueToString(context, _simpleValue)),
-//       ),
-//       itemBuilder: (context) => <PopupMenuItem<SimpleValue>>[
-//         PopupMenuItem<SimpleValue>(
-//           value: SimpleValue.one,
-//           child: Text(simpleValueToString(
-//             context,
-//             SimpleValue.one,
-//           )),
-//         ),
-//         PopupMenuItem<SimpleValue>(
-//           value: SimpleValue.two,
-//           child: Text(simpleValueToString(
-//             context,
-//             SimpleValue.two,
-//           )),
-//         ),
-//         PopupMenuItem<SimpleValue>(
-//           value: SimpleValue.three,
-//           child: Text(simpleValueToString(
-//             context,
-//             SimpleValue.three,
-//           )),
-//         ),
-//       ],
-//     );
-//   }
-// }
