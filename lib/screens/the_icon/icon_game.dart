56dfb2882e01d7871ad159567cb2c868a79c0ca0
Chi-Hong Cheung <chcheung.office@gmail.com>
2020-07-04 20:25:00 -0400

    Add in The Icons

--- lib/screens/the_icon/icon_game.dart
+++ lib/screens/the_icon/icon_game.dart
@@ -8,25 +8,28 @@ import 'package:game_app/screens/the_icon/components/game_title_bar.dart';
 import 'package:provider/provider.dart';
 
 class IconGame extends StatefulWidget {
+  final int gameVersion; // Either 0 (for The Icon) or 1 (for The Icons)
+  TheIconCore iconCore;
+  IconGame({Key key, @required this.gameVersion})
+      : iconCore = gameVersion == 0 ? TheIconCore() : TheIconsCore(),
+        super(key: key);
+
   @override
   _IconGameState createState() => _IconGameState();
 }
 
 class _IconGameState extends State<IconGame> {
-  TheIconCore iconCore = TheIconCore();
-  Color backgroundColor = Colors.blue[100];
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<TheIconCore>(
-        create: (context) => iconCore,
+        create: (context) => widget.iconCore,
         child: Scaffold(
-          backgroundColor: backgroundColor,
+          backgroundColor: widget.iconCore.scaffoldColor,
           appBar: AppBar(
-            title: Text("The Icon"),
-            backgroundColor: backgroundColor,
+            title: Text(widget.iconCore.getGameName()),
+            backgroundColor: widget.iconCore.scaffoldColor,
             elevation: 0,
-            leading: getUpdatorBackButton(context, iconCore),
+            leading: getUpdatorBackButton(context, widget.iconCore),
             actions: <Widget>[
               IconButton(
                 icon: Icon(Icons.help_outline),
@@ -34,7 +37,7 @@ class _IconGameState extends State<IconGame> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => InstructionsPage(iconCore),
+                      builder: (context) => InstructionsPage(widget.iconCore),
                     ),
                   );
                 },
@@ -54,22 +57,22 @@ class _IconGameState extends State<IconGame> {
 }
 
 // Exploring listview builder
-              // Container(
-              //   height: 150,
-              //   color: Colors.red,
-              //   alignment: Alignment.center,
-              //   child: ListView.separated(
-              //     padding: const EdgeInsets.all(8),
-              //     itemCount: 2,
-              //     scrollDirection: Axis.horizontal,
-              //     itemBuilder: (BuildContext context, int index) {
-              //       return Container(
-              //         height: 50,
-              //         color: Colors.amber[100],
-              //         child: Center(child: Text('E')),
-              //       );
-              //     },
-              //     separatorBuilder: (BuildContext context, int index) =>
-              //         const Divider(),
-              //   ),
-              // ),
+// Container(
+//   height: 150,
+//   color: Colors.red,
+//   alignment: Alignment.center,
+//   child: ListView.separated(
+//     padding: const EdgeInsets.all(8),
+//     itemCount: 2,
+//     scrollDirection: Axis.horizontal,
+//     itemBuilder: (BuildContext context, int index) {
+//       return Container(
+//         height: 50,
+//         color: Colors.amber[100],
+//         child: Center(child: Text('E')),
+//       );
+//     },
+//     separatorBuilder: (BuildContext context, int index) =>
+//         const Divider(),
+//   ),
+// ),
